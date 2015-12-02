d05561192a45c177e309cad8f8f5dd06b5ee5adc
Jóhann Haraldsson <joh19@login-0-2.local>
2015-12-02 13:29:47 +0000

    New run, save instance of nlptrain1.sh

--- nlptrain1.sh
+++ nlptrain1.sh
@@ -4,7 +4,7 @@
 #
 #These commands set up the Grid Environment for your job:
 # Request gpu queue, default queue is cpu and then nothing has to be specified
-#PBS -N nlptrain2cols
+#PBS -N rnnTwoColumndata_s256_d0.3
 #PBS -q gpu
 
 #load torch
@@ -14,4 +14,4 @@ module load torch/7
 cd /home/joh19/natlangproc/char-rnn
 
 #start training
-th train.lua -data_dir data/TwoColumndata -rnn_size 256 -num_layers 2
+th train.lua -data_dir data/TwoColumnData -rnn_size 256 -num_layers 2 -dropout 0.3
