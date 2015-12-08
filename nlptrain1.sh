fa9db2e2c5f02420f2e839cfc8ac7e892f781d72
Jóhann Haraldsson <joh19@login-0-2.local>
2015-12-08 17:54:45 +0000

    RNN train twocoldata, size:512, numlayers:3 (default is 2)

--- nlptrain1.sh
+++ nlptrain1.sh
@@ -4,7 +4,7 @@
 #
 #These commands set up the Grid Environment for your job:
 # Request gpu queue, default queue is cpu and then nothing has to be specified
-#PBS -N rnnTwoColumndata_s512d0.8
+#PBS -N rnnTwoColumndata_s512nl3
 #PBS -q gpu
 
 #load torch
@@ -14,4 +14,4 @@ module load torch/7
 cd /home/joh19/natlangproc/char-rnn
 
 #start training
-th train.lua -data_dir data/TwoColumnData -rnn_size 512 -num_layers 2 -dropout 0.8
+th train.lua -data_dir data/TwoColumnData -rnn_size 512 -num_layers 3 
