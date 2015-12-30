cac4cbacdfff09dadc1a56dfa41dd4511158afa2
joiharalds <johannhar@gmail.com>
2015-12-30 12:50:02 -0800

    Done modifying psuedodata.py to handle datapath with multiple files in --create. TODO: remove analysis.csv and wordderivatives.json from git since it is best to generate them with --derive and --human on the go.

--- pseudodata.py
+++ pseudodata.py
@@ -6,7 +6,7 @@ import os
 import csv
 
 def main(args):
-  words = dict()
+  fn_to_deriv = dict()
   jsonfn = 'wordderivatives.json'
   analysisfn = 'analysis.csv'
   pseudofn = 'pseudoinput.txt'
@@ -14,81 +14,106 @@ def main(args):
     #Path is relative to the directory which the script is run from
     path = args.derive
     filenames = getfilenames(path)
-    getderivatives(words,filenames,path)
-    writederivatives_to_json(words,jsonfn)
+    getderivatives(fn_to_deriv,filenames,path)
+    writederivatives_to_json(fn_to_deriv,jsonfn)
   if args.human:
-    if words:
-      writeanalysis(words,analysisfn)
+    if fn_to_deriv:
+      writeanalysis(fn_to_deriv,analysisfn)
     else:
       try:
         with open(jsonfn,'r') as f:
-          words = json.load(f)
-          writeanalysis(words,analysisfn)
+          fn_to_deriv = json.load(f)
+          writeanalysis(fn_to_deriv,analysisfn)
       except FileNotFoundError:
         print('No .json file was found, please use --derive to create one')
   if args.create:
-    #TODO: Receive path to datafiles as arguement as in args.derive
-    inputfn = args.create
-    if words:
-      createpseudodatafile(words,pseudofn,inputfn)
+    path = args.create
+    filenames = getfilenames(path)
+    if fn_to_deriv:
+      createpseudodatafiles(fn_to_deriv,filenames,path)
     else:
       try:
         with open(jsonfn,'r') as f:
-          words = json.load(f)
-          createpseudodatafile(words,pseudofn,inputfn)
+          fn_to_deriv = json.load(f)
+          createpseudodatafiles(fn_to_deriv,filenames,path)
       except FileNotFoundError:
         print('Either .json file was found then please use --derive to create one\
             or the input file was not found')
 
-def createpseudodatafile(words,pseudofn,inputfn):
+def createpseudodatafiles(fn_to_deriv,filenames,path):
+  """
+    Description: Iterate through all files in filenames and create a pseudodatafile
+      based on each of the files with the corresponding derivatives from fn_to_deriv.
+      The pseudodatafile for file1 will have the filename file1.pseudo
+  """
+  for filename in filenames:
+    createpseudodatafile(fn_to_deriv,filename,path)
+
+def createpseudodatafile(fn_to_deriv,inputfn,path):
   """
     Description: Creates a pseudodatafile with 2 columns; 'incorrectword,correctword'
       where incorrect word is chosen from 
+    Args:
+      inputfn: file to create pseudodata from
+      path: relative path from the scripts rundir to inputfn
   """
-  with open(pseudofn,'w') as pfhandle, open(inputfn,'r') as ihandle:
+  pseudodir = './pseudodata'
+  if not os.path.exists(pseudodir):
+    os.makedirs(pseudodir)
+  #Base name on first part of the name of input data file
+  pseudofn = inputfn.split('.')[0]+'.pseudo'
+  with open(pseudodir+'/'+pseudofn,'w') as pfhandle,\
+      open(path+'/'+inputfn,'r',newline='') as ihandle:
     ireader = csv.reader(ihandle,delimiter=',',quoting=csv.QUOTE_MINIMAL,quotechar='"')
     for row in ireader:
       try:
         word = row[1]
-        #Get list of derivatives
-        derivatives = list(words[word].keys())
-        #Generate random integer that is within the index of derivatives
-        i = random.randint(0,len(derivatives)-1)
-        derivative = derivatives[i]
+        #Get list of derivatives for word specific to the given file
+        derivatives = list(fn_to_deriv[inputfn][word].keys())
+        # **** Probability ****
+        #There is 4/7 probability that we choose from the list of possible derivatives
+        # for the given word. The correct word is included in this list.
+        l = [1,2,3,4,5,6,7]
+        if random.choice(l) > 3:
+          derivative = random.choice(derivatives)
+        else:
+          derivative = word
         pfhandle.write(derivative+','+word+'\n')
       except (KeyError,IndexError):
         #Word in input file was not seen when data was analyzed so we skip it
         #  from pseudodata. Skip over empty lines in input.txt
         continue
 
-def writeanalysis(words,fn):
+def writeanalysis(fn_to_deriv,fn):
   with open(fn,'w') as f:
-    for word,derivatives in words.items():
-      f.write(word)
-      for derivative,count in derivatives.items():
-        f.write(','+derivative+','+str(count))
-      f.write('\n')
+    for fn,words in fn_to_deriv.items():
+      for word,derivatives in words.items():
+        f.write(word)
+        for derivative,count in derivatives.items():
+          f.write(','+derivative+','+str(count))
+        f.write('\n')
 
-def writederivatives_to_json(words,fn):
+def writederivatives_to_json(fn_to_deriv,fn):
   """
     Description: Write derivative collection to json in directory of path.
   """
   with open(fn,'w') as f:
-    json.dump(words,f)
+    json.dump(fn_to_deriv,f)
 
-def getderivatives(words,filenames,path):
+def getderivatives(fn_to_deriv,filenames,path):
   """
     Description: Iterate through all files in filenames and get derivatives for
       each occuring word. Derivatives of a word are incorrect spellings of that word.
   """
   for filename in filenames:
-    getderivativesinfile(words,filename,path)
+    getderivativesinfile(fn_to_deriv,filename,path)
 
-def getderivativesinfile(words,fn,path):
+def getderivativesinfile(fn_to_deriv,fn,path):
   """
-    Description: Collect all words and derivatives seen in data along with count
+    Description: Collect all fn_to_deriv and derivatives seen in data along with count
+      Each file has its own set of derivatives to reduce noise when creating
+      pseudodata.
   """
-  #Boilerplate open call
   with open(path+'/'+fn,'r',newline='') as fhandle:
     reader = csv.reader(fhandle,delimiter=',',quoting=csv.QUOTE_MINIMAL,quotechar='"')
     #for row in itertools.islice(reader,0,5):
@@ -96,17 +121,23 @@ def getderivativesinfile(words,fn,path):
       try:
         word = row[1]
         derivative = row[0]
-        if word not in words:
+        #Remove ' from the set of possible derivatives since it causes problems
+        #  in Lua code
+        if derivative == "'":
+          continue
+        if fn not in fn_to_deriv:
+          fn_to_deriv[fn] = {word:{derivative:1}}
+        if word not in fn_to_deriv[fn]:
           #Word not seen before, add to dict
-          words[word] = {derivative:1}
-        elif derivative not in words[word]:
+          fn_to_deriv[fn][word] = {derivative:1}
+        elif derivative not in fn_to_deriv[fn][word]:
           #Word seen at least once before but derivative for word not seen before,
           #  add to subdict of word
-          words[word][derivative] = 1
+          fn_to_deriv[fn][word][derivative] = 1
         else:
           # Word and derivative both seen at least once before, count the occurence
-          derivativecount = words[word][derivative]
-          words[word][derivative] = derivativecount + 1
+          derivativecount = fn_to_deriv[fn][word][derivative]
+          fn_to_deriv[fn][word][derivative] = derivativecount + 1
       except IndexError:
         #IndexError probably created because some rows are empty so it is correct to
         #  ignore them
@@ -131,9 +162,9 @@ if __name__ == "__main__":
       fragmented in multiple data files and also concatenated into one large input.txt file\
       (but then make sure the input.txt file is the only file in the directory)')
   #Write analysis of the data in a human readable form
-  parser.add_argument('--human',action='store_true',help='Write words, derivatives and frequencies in human readable form .csv file')
+  parser.add_argument('--human',action='store_true',help='Write fn_to_deriv, derivatives and frequencies in human readable form .csv file')
   #Create pseudodata from input file
-  parser.add_argument('--create',metavar='INPUTFILE',help='Specify the two column input file from which to\
+  parser.add_argument('--create',metavar='DATAPATH',help='Specify the two column input file from which to\
       generate pseudodata, where incorrect word gets replaced by word derivative chosen with equal probability')
   args = parser.parse_args()
   main(args)
